ba9daf7d62ec9d257a728d80b25f54ddc30d4029
jyydhh <dcbajk@vip.qq.com>
2020-12-20 11:12:48 +0800

    first usable build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="myimpute",
-    version="0.0.1",
+    version="0.1.0",
     author="Shaobo Zhuang",
     license="MIT",
     author_email="dcbajk@vip.qq.com",
@@ -13,10 +13,13 @@ setuptools.setup(
     keywords=['Imputation', 'Missing Values', 'Missing', 'Linear', 'Mean'],
     long_description=long_description,
     long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(exclude=["tests.*", "tests"]),
     url="https://github.com/imjxydhh/MyImputation",
     classifiers=[
         'Natural Language :: English',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
         "License :: OSI Approved :: MIT License",
     ],
     python_requires='>=3.6',
